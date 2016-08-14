0ed9932df8bb1c06a2582b10da345718b3c1a78b
SKOLZ <gabriel.zanzotti@wolox.com.ar>
2016-08-14 13:24:25 -0300

    landing improvements.

--- vendorCss.js
+++ vendorCss.js
@@ -1,4 +1,5 @@
 module.exports = [
-  "bootstrap/dist/css/bootstrap.css",
-  "font-awesome/css/font-awesome.min.css"
+  // "bootstrap/dist/css/bootstrap.css",
+  "font-awesome/css/font-awesome.min.css",
+  "flexboxgrid/css/flexboxgrid.min.css"
 ];
