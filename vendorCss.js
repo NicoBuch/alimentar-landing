d8777febba8236e6192b33bed931a214b5470508
Nicolas Buchhalter <nico.buch.1010@gmail.com>
2016-08-26 15:27:16 -0300

    configuration fix for flexbox

--- vendorCss.js
+++ vendorCss.js
@@ -1,5 +1,5 @@
 module.exports = [
   // "bootstrap/dist/css/bootstrap.css",
   "font-awesome/css/font-awesome.min.css",
-  "flexboxgrid/css/flexboxgrid.min.css"
+  "flexboxgrid/css/index.min.css"
 ];
