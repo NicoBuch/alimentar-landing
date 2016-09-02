5f8d216e0c40bcb6983c5d7c8ff56bde6bdf8a6c
Nicolas Buchhalter <nico.buch.1010@gmail.com>
2016-09-02 18:21:21 -0300

    Send form by AJAX

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,16 @@
 
   console.log('WOLOX');
 
+
+  $('#form').submit(function() {
+    $.post(
+      'http://alimentar-mailing.herokuapp.com/mailing',
+      $(this).serialize()
+    ).done(function(){
+      $.growl.notice({ title: 'Gracias por enviarnos tu contacto!', message: 'La comida no se tira!' });
+      $(this).trigger('reset');
+    });
+    return false;
+  });
 })();
+
